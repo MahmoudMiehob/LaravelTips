3e726d189caada876df64cfefe2ec9d28450fba4
MahmoudMiehob <mahmoudmiehob@gmail.com>
2022-06-23 23:48:46 +0300

    add new feature to addPost

--- app/Http/Controllers/HttpClientController.php
+++ app/Http/Controllers/HttpClientController.php
@@ -9,5 +9,41 @@ class HttpClientController extends Controller
 {
 
 
+    
+
+
+    public function getAllPost(){
+        $posts = Http::get("https://jsonplaceholder.typicode.com/posts") ; 
+        return $posts->json() ; 
+    }
+
+    public function showPost($id){
+        $post = Http::get("https://jsonplaceholder.typicode.com/posts/".$id) ; 
+        return $post->json() ; 
+    }
+
+    public function addPost(Request $request){
+        $post = Http::post('https://jsonplaceholder.typicode.com/posts',[
+            'userId' => 1 ,
+            'title' => $request->title ,
+            'body' => $request->body 
+        ]) ; 
+        return $post->json() ; 
+    }
+
+    public function updatePost(){
+        $post = Http::put('https://jsonplaceholder.typicode.com/posts/1',[
+            'title' => 'hello' ,
+            'body' => 'mahmoud' 
+        ]) ; 
+        return $post->json() ; 
+    }
+
+    public function deletePost($id){
+        $post = Http::delete('https://jsonplaceholder.typicode.com/posts/'.$id);
+        return $post->json() ; 
+    }
+
+
 
 }
